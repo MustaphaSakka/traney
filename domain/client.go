0f322d7b2d7cdebff7cace455dc041ee17e573f5
Mustapha <sakka.mustaphaa@gmail.com>
2024-04-22 00:35:36 +0200

    add DTO logic for client :building_construction:

--- domain/client.go
+++ domain/client.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/MustaphaSakka/traney/exception"
+import (
+	"github.com/MustaphaSakka/traney/dto"
+	"github.com/MustaphaSakka/traney/exception"
+)
 
 type Client struct {
 	Id          string `db:"client_id"`
@@ -9,6 +12,25 @@ type Client struct {
 	Zipcode     string
 	DateofBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+func (c Client) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Client) ToDto() dto.ClientResponse {
+	return dto.ClientResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateofBirth: c.DateofBirth,
+		Status:      c.statusAsText(),
+	}
 }
 
 // Port
--- service/clientService.go
+++ service/clientService.go
@@ -2,25 +2,41 @@ package service
 
 import (
 	"github.com/MustaphaSakka/traney/domain"
+	"github.com/MustaphaSakka/traney/dto"
 	"github.com/MustaphaSakka/traney/exception"
 )
 
 // Port
 type ClientService interface {
-	GetAllClient() ([]domain.Client, error)
-	GetClient(string) (*domain.Client, *exception.AppException)
+	GetAllClient() ([]dto.ClientResponse, error)
+	GetClient(string) (*dto.ClientResponse, *exception.AppException)
 }
 
 type DefaultClientService struct {
 	repo domain.ClientRepository
 }
 
-func (s DefaultClientService) GetAllClient() ([]domain.Client, error) {
-	return s.repo.FindAll()
+func (s DefaultClientService) GetAllClient() ([]dto.ClientResponse, error) {
+	clients, err := s.repo.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	response := make([]dto.ClientResponse, 0)
+	for _, c := range clients {
+		response = append(response, c.ToDto())
+	}
+	return response, err
+
 }
 
-func (s DefaultClientService) GetClient(id string) (*domain.Client, *exception.AppException) {
-	return s.repo.FindById(id)
+func (s DefaultClientService) GetClient(id string) (*dto.ClientResponse, *exception.AppException) {
+	c, err := s.repo.FindById(id)
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+	return &response, nil
+
 }
 
 func NewClientService(repository domain.ClientRepository) DefaultClientService {
