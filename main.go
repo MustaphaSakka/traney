57c807dc8108390b15ede66defcd81d3a7a00fc6
Mustapha SAKKA <sakka.mustaphaa@gmail.com>
2026-09-21 21:03:54 +0200

    feat: configure database connection with environment variables

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/MustaphaSakka/traney-lib/logger"
 	"github.com/MustaphaSakka/traney/app"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	logger.Info("TRANEY is started!")
 	app.Start()
 
